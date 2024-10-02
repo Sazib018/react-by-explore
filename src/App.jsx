bb32382c1fbe88260fcc451d28574c3ac405c910
Sazib <mahiburislamsazib@gmail.com>
2024-10-02 23:27:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import Person from "./componets/Person/Person";
+import StudentInfo from "./componets/StudentInfo/Studentsinfo";
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="mb-6">
+        <StudentInfo name="Asif" age="24" isMale={true}></StudentInfo>
+      </div>
+      <div className="mb-6">
+        <StudentInfo name="simu" age="20" isMale={false}></StudentInfo>
+      </div>
+      <div className="mb-6">
+        <StudentInfo name="Sazib" age="26" isMale={true}></StudentInfo>
+      </div>
+      <Person></Person>
+    </>
+  );
+}
+
+
+export default App;
